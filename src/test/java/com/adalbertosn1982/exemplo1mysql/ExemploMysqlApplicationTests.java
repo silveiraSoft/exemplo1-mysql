17867e69494e38c7890c946bb9828d4af68aa746
Workshop User <asilveira@3htp.com>
2024-09-07 19:56:55 -0400

    first commit

---
+++ src/test/java/com/adalbertosn1982/exemplo1mysql/ExemploMysqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adalbertosn1982.exemplo1mysql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExemploMysqlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
